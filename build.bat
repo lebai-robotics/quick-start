a352a9ce37e442931dfdac482d47c574c49ea977
Kingfree <kingfree@toyama.moe>
2020-12-02 11:29:53 +0800

    1202上午

--- build.bat
+++ build.bat
@@ -5,4 +5,4 @@
 @REM For /f "tokens=1-3 delims=-/. " %j In ('Date /T') do copy user-manual.pdf 乐白LM3用户手册-正文-%k%l.pdf
 xelatex user-manual
 xelatex user-manual
-For /f "tokens=1-3 delims=-/. " %%j In ('Date /T') do copy user-manual.pdf 乐白LM3用户手册-v1.0.2-%%k%%l.pdf
+For /f "tokens=1-3 delims=-/. " %%j In ('Date /T') do copy user-manual.pdf 乐白LM3用户手册-v1.0.3-%%k%%l.pdf
